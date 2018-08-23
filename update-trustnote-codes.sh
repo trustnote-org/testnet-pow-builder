ec0e8dc616d5310279f4073a5cdd5a558e465697
yiyanwannian <774392980@qq.com>
2018-08-23 11:18:02 +0800

    add part update

--- update-trustnote-codes.sh
+++ update-trustnote-codes.sh
@@ -29,11 +29,27 @@ update_trustnote_pow(){
     update_trustnote_pow_cleandb "$2" "$3" "$4"
 }
 
-for index in {1..9}
-do
-    update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "$index"
-done
+start_all_supernode(){
+    for index in {1..9}
+    do
+        update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "$index"
+    done
+}
 
+start_part_supernode(){
+    update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "1"
+    update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "2"
+    update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "3"
+    update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "4"
+    update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "5"
+    update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-supernode" "9"
+}
+
+#start_all_supernode
+start_part_supernode
+
+update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-headless" "10"
+update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-explorer" "1"
 
 update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-headless" "10"
 update_trustnote_pow "$source_path" "$config_path" "trustnote-pow-explorer" "1"
