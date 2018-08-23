e2aa61a07c80b6828aa6303614f90f948b8296ad
yiyanwannian <774392980@qq.com>
2018-08-23 11:11:49 +0800

    add pm2 stop script

---
+++ create_all_supernodes_to_pm2.sh
@@ -0,0 +1,11 @@
+pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode1/start.js  --name supernode1
+pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode2/start.js  --name supernode2
+pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode3/start.js  --name supernode3
+pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode4/start.js  --name supernode4
+pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode5/start.js  --name supernode5
+#pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode6/start.js  --name supernode6
+#pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode7/start.js  --name supernode7
+#pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode8/start.js  --name supernode8
+pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode9/start.js  --name supernode9
+
+pm2 start ~/mainchain-pow/testnet-pow-builder/trustnote-pow-explorer1/explorer.js  --name explorer1
