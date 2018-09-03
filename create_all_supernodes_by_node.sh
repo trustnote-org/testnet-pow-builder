ab98d21ab9bca438950f7cebabb029e596cfef22
unknown <774392980@qq.com>
2018-09-03 11:13:40 +0800

    add start with node style

---
+++ create_all_supernodes_by_node.sh
@@ -0,0 +1,11 @@
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode1/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode2/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode3/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode4/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode5/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode6/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode7/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode8/start.js &
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-supernode9/start.js &
+
+node ~/mainchain-pow/testnet-pow-builder/trustnote-pow-explorer1/explorer.js &
