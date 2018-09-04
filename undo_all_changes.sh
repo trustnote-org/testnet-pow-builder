b5da1392078aef517e66108f8f51efb63dae4e50
unknown <774392980@qq.com>
2018-09-04 11:03:47 +0800

    refactor undo_all_changes code

--- undo_all_changes.sh
+++ undo_all_changes.sh
@@ -1,30 +1,9 @@
-git -C trustnote-pow-supernode1 checkout .
-git -C trustnote-pow-supernode1 pull
 
-git -C trustnote-pow-supernode2 checkout .
-git -C trustnote-pow-supernode2 pull
-
-git -C trustnote-pow-supernode3 checkout .
-git -C trustnote-pow-supernode3 pull
-
-git -C trustnote-pow-supernode4 checkout .
-git -C trustnote-pow-supernode4 pull
-
-git -C trustnote-pow-supernode5 checkout .
-git -C trustnote-pow-supernode5 pull
-
-git -C trustnote-pow-supernode6 checkout .
-git -C trustnote-pow-supernode6 pull
-
-git -C trustnote-pow-supernode7 checkout .
-git -C trustnote-pow-supernode7 pull
-
-git -C trustnote-pow-supernode8 checkout .
-git -C trustnote-pow-supernode8 pull
-
-git -C trustnote-pow-supernode9 checkout .
-git -C trustnote-pow-supernode9 pull
-
+for index in {1..9}
+do
+    git -C "trustnote-pow-supernode$index" checkout .
+    git -C "trustnote-pow-supernode$index" pull
+done
 
 git -C trustnote-pow-supernode checkout .
 git -C trustnote-pow-supernode pull
